a063184180fa70da6accaaddb297b9773a61b86f
Your Waktariya <vikasdhakad989@gmail.com>
2020-12-03 11:55:56 +0530

    setup gmail

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,11 +1,13 @@
 ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.default :charset => "utf-8"
 ActionMailer::Base.smtp_settings = {
-  address:         'smtp.gmail.com',
-  domain:          'mail.google.com',
-  port:            587,
-  user_name:       'vikasdhakad989@gmail.com',
-  password:        'Va8817145293Va@8817145293',
-  authentication:  'plain'
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => 'smtp.gmail.com',
+  :user_name => "vikasdhakad989@gmail.com",
+  :password => "Va8817145293Va8817145293",
+  :authentication => :login,
+  :enable_starttls_auto => true
 }
